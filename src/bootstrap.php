0fb8841c3230b71dcc333878528e358d609b447b
alizizo888-code <alizizo888@gmail.com>
2026-09-25 07:52:32 +0300

    Wire order lifecycle into Unit 02

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 defined('ABSPATH') || exit;
 require_once MUTQAN_DIR.'src/Unit02/class-mutqan-operations.php';
+require_once MUTQAN_DIR.'src/Unit02/class-mutqan-orders.php';
 require_once MUTQAN_DIR.'src/Unit03/class-mutqan-services.php';
 require_once MUTQAN_DIR.'src/Unit04/class-mutqan-finance.php';
 require_once MUTQAN_DIR.'src/Unit05/class-mutqan-marketing.php';
