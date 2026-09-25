8c49f11fc8009e112b83600a044f1b877900e0ff
alizizo888-code <alizizo888@gmail.com>
2026-09-25 07:55:26 +0300

    Wire GPS storage into Unit 02 activation

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,6 +3,7 @@ defined('ABSPATH') || exit;
 require_once MUTQAN_DIR.'src/Unit02/class-mutqan-operations.php';
 require_once MUTQAN_DIR.'src/Unit02/class-mutqan-orders.php';
 require_once MUTQAN_DIR.'src/Unit02/class-mutqan-dispatch.php';
+require_once MUTQAN_DIR.'src/Unit02/class-mutqan-gps.php';
 require_once MUTQAN_DIR.'src/Unit03/class-mutqan-services.php';
 require_once MUTQAN_DIR.'src/Unit04/class-mutqan-finance.php';
 require_once MUTQAN_DIR.'src/Unit05/class-mutqan-marketing.php';
@@ -15,6 +16,7 @@ require_once MUTQAN_DIR.'src/Core/class-mutqan-registry-admin.php';
 require_once MUTQAN_DIR.'src/Core/class-mutqan-permissions-admin.php';
 register_activation_hook(MUTQAN_FILE,function(){
     MUTQAN_Operations::install();
+    MUTQAN_GPS::install();
     MUTQAN_Services::install();
     MUTQAN_Finance::install();
     MUTQAN_Marketing::install();
