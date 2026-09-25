81260311f07500db12ebf1fdbd4108e67a87574e
alizizo888-code <alizizo888@gmail.com>
2026-09-25 07:32:53 +0300

    Wire Unit 01 into MUTQAN bootstrap

--- mutqan.php
+++ mutqan.php
@@ -12,5 +12,11 @@ define('MUTQAN_FILE', __FILE__);
 define('MUTQAN_DIR', plugin_dir_path(__FILE__));
 
 require_once MUTQAN_DIR . 'src/Core/class-mutqan-core.php';
+require_once MUTQAN_DIR . 'src/Unit01/class-mutqan-users.php';
+require_once MUTQAN_DIR . 'src/Unit01/class-mutqan-audit.php';
+require_once MUTQAN_DIR . 'src/Unit01/class-mutqan-admin.php';
+require_once MUTQAN_DIR . 'src/Unit01/bootstrap.php';
+
+register_activation_hook(MUTQAN_FILE, array('MUTQAN_Unit01', 'activate'));
 
 add_action('plugins_loaded', array('MUTQAN_Core', 'boot'));
