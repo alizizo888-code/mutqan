385025fecce9322fc04c9392e71d6bb4630edbfc
alizizo888-code <alizizo888@gmail.com>
2026-09-25 09:40:51 +0300

    MUTQAN: prepare integrated release 0.2.0

--- mutqan.php
+++ mutqan.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name: MUTQAN Unified System
  * Description: Unified maintenance operations core for WordPress.
- * Version: 0.1.1
+ * Version: 0.2.0
  * Requires PHP: 7.4
  */
 defined('ABSPATH') || exit;
 
-define('MUTQAN_VERSION', '0.1.1');
+define('MUTQAN_VERSION', '0.2.0');
 define('MUTQAN_FILE', __FILE__);
 define('MUTQAN_DIR', plugin_dir_path(__FILE__));
 
